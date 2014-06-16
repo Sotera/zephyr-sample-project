50ebc95518afac13e74dd2f15a2844b1af381ead
Dwayne Pryce <dwayne.pryce@soteradefense.com>
2014-06-16 14:19:29 -0700

    Committing the changes to 0.2.0 and adding support for different versions of CDH

--- sample-project-mapreduce/dist/run.sh
+++ sample-project-mapreduce/dist/run.sh
@@ -8,4 +8,4 @@ jar -cf lib/resources.jar -C conf/ .
 
 export HADOOP_CLASSPATH=lib/*:$HADOOP_CLASSPATH
 
-hadoop jar lib/zephyr-mapreduce-0.1.0.jar org.zephyr.mapreduce.ZephyrDriver "${@:1}"
+hadoop jar lib/zephyr-mapreduce-0.2.0.jar org.zephyr.mapreduce.ZephyrDriver "${@:1}"
